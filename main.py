d804f3bc70ff97f3f9e8eb7138c04aa7767e79fa
Vivek Chandan <vivekchandan73@gmail.com>
2025-06-18 11:57:50 +0530

    Initial CI/CD setup

---
+++ main.py
@@ -0,0 +1,6 @@
+# main.py
+def print_name():
+    print("Hello, my name is Vivek")
+
+if __name__ == "__main__":
+    print_name()
---
+++ test_main.py
@@ -0,0 +1,3 @@
+# test_main.py
+def test_output():
+    assert True  # Placeholder, just to simulate a passing test
